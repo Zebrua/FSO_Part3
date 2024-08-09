ff23628c7a7ddba01bdbbac9bbd2f634b5237435
Zebrua <141924342+Zebrua@users.noreply.github.com>
2024-08-09 11:18:58 +0300

    Create 3-3_index.js

---
+++ 3-3_index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let phonebook = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(phonebook)
+})
+
+app.get('/info', (request,response) => {
+    response.send(
+        `<p>Phonbook has ${phonebook.length} people<br/>
+        ${new Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = phonebook.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+    }
+})
+
+const PORT = 3005
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})

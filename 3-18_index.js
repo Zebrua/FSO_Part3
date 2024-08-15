6c25e007caef3b4cf43b3dd0cb16ac7189ca73f2
Zebrua <141924342+Zebrua@users.noreply.github.com>
2024-08-15 11:56:21 +0300

    Create 3-18_index.js

---
+++ 3-18_index.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const app = express()
+dotenv.config()
+
+const morgan = require('morgan')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+})
+const Phonenumber = mongoose.model('Phonenumber', personSchema)
+
+morgan.token('data', function getData (req) {
+    const body = req.body
+    return JSON.stringify(body)
+})
+
+app.use(express.static('dist'))
+app.use(cors())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(express.json())
+
+// const baseURL ='/api/persons';
+
+app.get('/hello', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    Phonenumber.find({}).then(phonebook => {
+        response.json(phonebook)
+    })
+})
+
+app.get('/info', (request,response) => {
+    Phonenumber.find()
+    .then(phonebook => {
+        response.send(
+        `<p>Phonbook has ${phonebook.length} people<br/>
+        ${new Date()}</p>`)
+    })
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    Phonenumber.findById(id)
+    .then(person => { 
+        response.json(person)
+    }) 
+    .catch((error) => {
+        response.status(400).end()
+    })
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    const id = request.params.id
+    Phonenumber.findByIdAndDelete(id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request,response, next) => {
+    const id = request.params.id
+    const body = request.body
+    Phonenumber.findByIdAndUpdate(id, {number: body.number})
+    .then(result => { response.status(204).end() } )
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+            error: 'required content is missing' 
+        })
+    }
+
+    const phonenumber = new Phonenumber({
+        name: body.name,
+        number: body.number
+    })
+    
+    phonenumber.save().then(savedResult => {response.json(savedResult)})
+    
+})
+
+const PORT = process.env.PORT || 3005
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

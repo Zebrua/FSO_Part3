1d1df1832ec1cce5cba8fa16302e2a9c27b1f174
Zebrua <141924342+Zebrua@users.noreply.github.com>
2026-08-02 12:17:35 +0300

    Update 3-6_index.js

--- 3-6_index.js
+++ 3-6_index.js
@@ -80,7 +80,7 @@ app.post('/api/persons', (request, response) => {
     response.json(phonebook)
 })
 
-const PORT = 3005
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

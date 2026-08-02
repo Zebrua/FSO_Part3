be5c63b51dfc3305db688586fa8d02d9d1c698f4
Zebrua <141924342+Zebrua@users.noreply.github.com>
2026-08-02 12:03:40 +0300

    Change server port from 3005 to 3001

--- 3-1_index.js
+++ 3-1_index.js
@@ -34,7 +34,7 @@ app.get('/api/persons', (request, response) => {
   response.json(phonebook)
 })
 
-const PORT = 3005
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
